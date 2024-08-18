5b145b18957051d5ab1e05cb885784978c14152b
hasank34 <hasan3846fb@gmail.com>
2024-08-18 15:54:29 +0300

    first commit

---
+++ src/utils/api.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+
+const api = axios.create({ baseURL: "http://localhost:4040"});
+
+axios.get ("http://localhost:4040/todos");
+api.get("/todos");
+
+export default api;
